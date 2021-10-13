4b877185cfae2f699a8d3ee0ada66b99ebfa72e3
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-13 23:40:04 +0530

    Added: by useing compareTo we find the maximum of three Integers

--- src/main/java/com/bdlz/generics/Maximum.java
+++ src/main/java/com/bdlz/generics/Maximum.java
@@ -1,7 +1,20 @@
 package com.bdlz.generics;
 
 public class Maximum {
+
+    public static Integer findMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(a)>0 && b.compareTo(c)>0) {
+            max = b;
+        } if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println(max);
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome Generics Maximum program");
+        Maximum.findMaximum(9,4,6);
     }
 }
